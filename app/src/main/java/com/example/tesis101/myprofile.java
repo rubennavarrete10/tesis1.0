a8482810d7e897a2cb3ceddb1293cb80c2c6ce47
owner <rubendiaznt@live.com.mx>
2019-10-10 10:14:08 -0500

    todo bien arreglos visuales, vibracion al reconocer checkbox algo

--- app/src/main/java/com/example/tesis101/myprofile.java
+++ app/src/main/java/com/example/tesis101/myprofile.java
@@ -1,27 +1,60 @@
 package com.example.tesis101;
-
-import android.app.Activity;
-import android.content.Intent;
+import android.widget.CheckBox;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
+import android.os.Handler;
+
 
 public class myprofile extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+    private CheckBox chek1;
+    private CheckBox chek2;
+    private CheckBox chek3;
+    TextView valorA;
+    String valoralarma;
+    int tiempoalarma=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myprofile);
 
-        Button btn2 = (Button) findViewById(R.id.button2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2 = new Intent (v.getContext(), MainActivity.class);
-                startActivityForResult(intent2, 0);
+            Button btn2 = (Button) findViewById(R.id.button2);
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent2 = new Intent(v.getContext(), MainActivity.class);
+                    startActivityForResult(intent2, 0);
+                }
+            });
+
+            chek1 = (CheckBox) findViewById(R.id.checkBox3);
+            chek2 = (CheckBox) findViewById(R.id.checkBox4);
+            chek3 = (CheckBox) findViewById(R.id.checkBox5);
+            valorA = (TextView) findViewById(R.id.textView4);
+
+            String s = "Estado: " + (chek1.isChecked() ? "Marcado" : "No Marcado");
+            Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+
+            if (chek1.isChecked() == true) {
+                tiempoalarma = 10;
+                valoralarma = "1 segundo";
+                chek2.setChecked(false);
+                chek3.setChecked(false);
             }
-        });
+            else
+            {
+                valoralarma = "N/A";
+            }
+            valorA.setText(valoralarma);
+
+
     }
-}
+    }
--- app/src/main/java/com/example/tesis101/reconocimientoVoz.java
+++ app/src/main/java/com/example/tesis101/reconocimientoVoz.java
@@ -23,75 +23,70 @@ public class reconocimientoVoz extends AppCompatActivity {
     TextView grabar;//
     private Vibrator vibrator;//VARIABLE DE LA ALARMA
     private static final int RECOGNIZE_SPEECH_ACTIVITY = 1;
-
+    int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recvoz);
 
+
         Button btn3 = (Button) findViewById(R.id.button4);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent3 = new Intent (v.getContext(), MainActivity.class);
+                Intent intent3 = new Intent(v.getContext(), MainActivity.class);
                 startActivityForResult(intent3, 0);
             }
         });
 
-        grabar = (TextView) findViewById(R.id.textView3);
-        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            grabar = (TextView) findViewById(R.id.textView3);
+            vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
-    }
+            Intent intentActionRecognizeSpeech = new Intent(
+                    RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intentActionRecognizeSpeech.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-MX");
+            try {
+                startActivityForResult(intentActionRecognizeSpeech,
+                        RECOGNIZE_SPEECH_ACTIVITY);
+            } catch (ActivityNotFoundException a) {
+                Toast.makeText(getApplicationContext(),
+                        "Tú dispositivo no soporta el reconocimiento por voz",
+                        Toast.LENGTH_SHORT).show();
+            }
+            a = a + 1;
 
-
+        }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
 
+            switch (requestCode) {
+                case RECOGNIZE_SPEECH_ACTIVITY:
+                    if (resultCode == RESULT_OK && null != data) {
+                        ArrayList<String> speech = data
+                                .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                        String strSpeech2Text = speech.get(0);
+                        grabar.setText(strSpeech2Text);
 
-        switch (requestCode) {
-            case RECOGNIZE_SPEECH_ACTIVITY:
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> speech = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String strSpeech2Text = speech.get(0);
-                    grabar.setText(strSpeech2Text);
+                        if (vibrator.hasVibrator()) //Si tiene vibrador//
+                        {
+                            //vibra N milisegundos
+                            if (true) {
+                                long tiempo = 500;
+                                vibrator.vibrate(tiempo);
+                            }
 
-                    if (vibrator.hasVibrator()) //Si tiene vibrador//
-                    {
-                        //vibra N milisegundos
-                        if (true){
-                            long tiempo = 500;
-                            vibrator.vibrate(tiempo);
                         }
-
+                        a=0;
                     }
-
-                }
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
-    }
-    public void onClickImgBtnHablar(View v) {
-        Intent intentActionRecognizeSpeech = new Intent(
-                RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-
-        // Configura el Lenguaje (Español-México)
-        intentActionRecognizeSpeech.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE_MODEL, "es-MX");
-        try {
-            startActivityForResult(intentActionRecognizeSpeech,
-                    RECOGNIZE_SPEECH_ACTIVITY);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(),
-                    "Tú dispositivo no soporta el reconocimiento por voz",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
 
 }
